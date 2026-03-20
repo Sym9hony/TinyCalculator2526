f4c93dbd64d35fa51eaebe691906e926e7593f83
Moritz Noah <N.Moritz@htlvb.at>
2026-03-20 11:13:33 +0100

    Added power operand

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -14,14 +14,7 @@ Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine($"{a} * {b} = {a * b}");
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"{a} / {b} = {(double)a / b}");
-//if (b == 0)
-//{ 
-//    Console.WriteLine($"{a} / {b} = undefied");
-//}
-//else
-//{
-//    Console.WriteLine($"{a} / {b} = {(double)a / b}");
-//}
+
 Console.ResetColor();
 
 static int ReadOperand(string title)
