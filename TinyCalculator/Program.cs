bd322056fed0be69a032ab1e200bf2a70db8fae1
Moritz Noah <N.Moritz@htlvb.at>
2026-03-20 09:09:29 +0100

    Add color to the calculations

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,13 +1,19 @@
 ﻿Console.WriteLine("=== Tiny Calculator ===");
 
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.Write("Operand 1: ");
 int a = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Operand 2: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
+Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine($"{a} + {b} = {a + b}");
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"{a} - {b} = {a - b}");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
 Console.WriteLine($"{a} * {b} = {a * b}");
+Console.ForegroundColor = ConsoleColor.Green;
 if (b == 0)
 { 
     Console.WriteLine($"{a} / {b} = undefied");
@@ -16,7 +22,7 @@ else
 {
     Console.WriteLine($"{a} / {b} = {a / b}");
 }
-
+Console.ResetColor();
 
 
 
