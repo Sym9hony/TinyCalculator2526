2c15e0d194c23d50ef7bac202658f0df7e218091
Moritz Noah <N.Moritz@htlvb.at>
2026-03-20 08:06:05 +0100

    Read operands from console

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,6 +1,11 @@
 ﻿Console.WriteLine("=== Tiny Calculator ===");
 
-int a = 5;
-int b = 10;
+Console.Write("Summand 1: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Summand 2: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
 
 Console.WriteLine($"{a} + {b} = {a + b}");
+
+
+
