0260ee992748e900267082b4f0e66c462783ba11
Moritz Noah <N.Moritz@htlvb.at>
2026-03-20 08:36:28 +0100

    Show calculation result for all basic operators

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,11 +1,14 @@
 ﻿Console.WriteLine("=== Tiny Calculator ===");
 
-Console.Write("Summand 1: ");
+Console.Write("Operand 1: ");
 int a = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Summand 2: ");
+Console.Write("Operand 2: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
 
 Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+Console.WriteLine($"{a} / {b} = {a / b}");
 
 
 
